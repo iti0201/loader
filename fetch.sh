92998d1526d0a959fdd18894ff6f0e79b16f5bf2
Gert Kanter <gert.kanter@robokratt.xyz>
2018-09-04 13:30:29 +0300

    loader

---
+++ fetch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: fetch.sh [robot-id] [uni-id]"
+    exit
+fi
+echo "Cloning your repository..."
+git clone https://$2@gitlab.cs.ttu.ee/$2/iti0201.git student
+echo "Copying your output from the robot..."
+sshpass -p 'wellwellwellhellothere' scp drop@192.168.0.$1:/dropzone/output.txt .
+TIMESTAMP=$(date +%s)
+rm -rf student/logs
+mkdir student/logs
+mv output.txt student/logs/$TIMESTAMP.txt
+cd student
+git add logs/$TIMESTAMP.txt
+git config user.email "robottester@ttu.ee"
+git config user.name "Robot Tester"
+git commit -m "Output from robot test run @ $TIMESTAMP"
+echo "Pushing the log file to your repository..."
+git push origin master
+echo "Removing your files..."
+cd ..
+rm -rf student/
---
+++ load.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Usage: load.sh [robot-id] [task-id] [uni-id]"
+    exit
+fi
+rm -rf student/
+echo "Cloning your repository..."
+git clone https://$3@gitlab.cs.ttu.ee/$3/iti0201-2018.git student
+echo "Copying your files to the robot..."
+sshpass -p 'plaintextparoolrukiddingme???' scp student/$2/robot.py drop@192.168.0.$1:/dropzone
+echo "Removing your files..."
+rm -rf student/
+echo "Thank you, come again!"
