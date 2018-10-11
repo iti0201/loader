dea3323b4c677b35a71bf19f264552581d25935c
Gert Kanter <gert.kanter@robokratt.xyz>
2018-10-11 09:30:08 +0300

    tail dropzone

--- output.sh
+++ output.sh
@@ -3,4 +3,4 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: output.sh [robot-id]"
     exit
 fi
-sshpass -p 'passincleartextnoway' ssh drop@192.168.0.9$1 "tail -f /home/pi/output.txt"
+sshpass -p 'passincleartextnoway' ssh drop@192.168.0.9$1 "tail -f /home/pi/output.txt /dropzone/output.txt"
